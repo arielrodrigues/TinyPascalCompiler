52a0a81a69e8a0267f89fb495b079babbb48c7f0
analuisap <alupires_@hotmail.com>
2017-02-05 22:34:51 -0300

    pull

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.io.StringReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String expressao = "if 2+3 := 5 then 5";
+        String expressao = "begin Begin BEGin if 2+3 := 5 then 5";
         LexicalAnalyzer lex = new LexicalAnalyzer(new StringReader(expressao));
         lex.yylex();
     }
