05a9ba99a9a94dbdb16ea5bec6d3cb0dcfcbcf40
Ariel <arielrodrigues@protonmail.com>
2017-03-12 23:08:56 -0300

    add lex and cup files

--- src/LexicalAnalyzer/JFlexHandler/LexicalAnalyzerGenerator.java
+++ src/LexicalAnalyzer/JFlexHandler/LexicalAnalyzerGenerator.java
@@ -1,12 +1,13 @@
-/*package LexicalAnalyzer.JFlexHandler;
+package LexicalAnalyzer.JFlexHandler;
 
 import java.io.File;
 import java.nio.file.Paths;
 
 public class LexicalAnalyzerGenerator {
     public static void main(String[] args)	{
-    	String filename = Paths.get("").toAbsolutePath().toString() + "/src/LexicalAnalyzer/" + "pascal.lex";
+    	String filename = Paths.get("").toAbsolutePath().toString() + "/src/" + "pascal.lex";
         File sourceCode = new File(filename);
         jflex.Main.generate(sourceCode);
+
     }
-}*/
+}
--- src/Main.java
+++ src/Main.java
@@ -23,3 +23,10 @@ public class Main {
     }
 }
 */
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Parser parser = new Parser();
+		parser.parse();
+	}	
+}
