ea60dedc7172d2606380ecfe7f2886cbe71cf107
Ariel <arielrodrigues@protonmail.com>
2017-03-25 00:39:45 -0300

    some modifications

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-/*
-
-import utils.FileManager;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.StringReader;
-import java.nio.file.Paths;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        FileManager fileManager = new FileManager();
-        StringBuilder stringBuilder = new StringBuilder();
-        String inFile = Paths.get("").toAbsolutePath().toString()+"/src/"+"input.txt";
-		LexicalAnalyzer lex = new LexicalAnalyzer(new StringReader(inFile.toString()));
-		lex.yylex();
-
-        LexicalAnalyzer scanner = new LexicalAnalyzer(new FileReader(inFile));
-    }
-}*/
+import syntacticAnalyzer.Parser;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Parser parser = new Parser();
-		parser.parse();
+		Parser syntacticAnalyzer = new Parser();
+		syntacticAnalyzer.parse();
 	}	
 }
