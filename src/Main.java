9c234291ab559f17afdf744c1bb76fcc5f062524
Ariel <arielrodrigues@protonmail.com>
2017-03-30 20:41:14 -0300

    pretty print test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import abstractSyntax.Node.Program;
 import syntacticAnalyzer.Parser;
+import visitor.PrettyprintVisitor;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Parser syntacticAnalyzer = new Parser();
-		syntacticAnalyzer.parse();
+		PrettyprintVisitor prettyPrint = new PrettyprintVisitor((Program) syntacticAnalyzer.parse().value);
 	}	
 }
