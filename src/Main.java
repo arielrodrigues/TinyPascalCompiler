b8148648ff0c8eab94fc0cd9502d5d6373fa73ab
Ariel <arielrodrigues@protonmail.com>
2017-04-02 01:34:21 -0300

    some updates

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import abstractSyntax.Node.Program;
 import syntacticAnalyzer.Parser;
+import visitor.PascalVisitor;
 import visitor.PrettyprintVisitor;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Parser syntacticAnalyzer = new Parser();
-		PrettyprintVisitor prettyPrint = new PrettyprintVisitor((Program) syntacticAnalyzer.parse().value);
+		//syntacticAnalyzer.debug_parse();
+		PascalVisitor prettyPrint = new PrettyprintVisitor((Program) syntacticAnalyzer.parse().value);
 	}	
 }
