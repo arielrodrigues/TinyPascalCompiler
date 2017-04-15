2a95b596d8f09bf30beb457f487877fe49ea0b26
Ariel <arielrodrigues@protonmail.com>
2017-04-15 19:48:37 -0300

    tests with type checker

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ import abstractSyntax.Node.Program;
 import syntacticAnalyzer.Parser;
 import visitor.PascalVisitor;
 import visitor.PrettyPrint;
+import visitor.TypeChecker;
+
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Parser syntacticAnalyzer = new Parser();
 		//syntacticAnalyzer.debug_parse();
-		PascalVisitor prettyPrint = new PrettyPrint((Program) syntacticAnalyzer.parse().value);
+		Program pasProgram = (Program) syntacticAnalyzer.parse().value;
+		PascalVisitor prettyPrint = new PrettyPrint(pasProgram);
+		TypeChecker typeChecker = new TypeChecker(pasProgram);
 	}	
 }
