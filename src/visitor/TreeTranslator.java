0c757f43835f5e286ddcc5f4b6a04e7e4ce98e05
Ariel <arielrodrigues@protonmail.com>
2017-04-29 15:26:38 -0300

    update intermediate code translator (tree)

--- src/visitor/TreeTranslator.java
+++ src/visitor/TreeTranslator.java
@@ -1,5 +1,8 @@
 package visitor;
 
+import IntermediateRepresentation.Binding.*;
+import IntermediateRepresentation.Temp.*;
+import IntermediateRepresentation.Tree.*;
 import abstractSyntax.ConformantArray.*;
 import abstractSyntax.Constant.*;
 import abstractSyntax.Exp.*;
@@ -10,8 +13,23 @@ import abstractSyntax.Stm.*;
 import abstractSyntax.labelsAndTypes.*;
 import abstractSyntax.procedureAndFunctionDeclaration.*;
 import abstractSyntax.variablesDeclaration.*;
+import utils.symbol.*;
+import utils.symbol.Exceptions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TreeTranslator implements PascalVisitor {
+
+    int currentLevel;
+    Frame currentFrame;
+
+    SymbolTable<Binding> env = new SymbolTable<>();
+
+    //Each function and procedure is translated into a SubprogramSegment, thus
+    //the visit to a Program returns the correspondent List<SubprogramSegment>
+    //For the sake of simplicity, we assume that the main program has a frame
+    //which hosts the global variables.
 
     public TreeTranslator (Program prog) {
         VisitProgram(prog);
@@ -19,52 +37,86 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitConformantArrayParameter(ConformantArrayParameter conformantArrayParameter) {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object VisitMultiDimensionConformant(MultiDimensionConformant multiDimensionConformant) {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object VisitOneDimensionConformant(OneDimensionConformant oneDimensionConformant) {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object VisitConstantId(IdConstant idConstant) {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object VisitBooleanConstant(BooleanConstant booleanConstant) {
-        return null;
+        return booleanConstant.value? new CONST(1) : new CONST(0);
     }
 
     @Override
     public Object VisitCharacterConstant(CharacterConstant characterConstant) {
-        return null;
+        return new CONST((int) characterConstant.value);
     }
 
     @Override
     public Object VisitSignedNumber(SignedNumber signedNumber) {
-        return null;
+        return signedNumber.sign == Sign.PLUS?
+                new CONST(signedNumber.unsNum.value) :
+                    new CONST(-signedNumber.unsNum.value);
     }
 
     @Override
     public Object VisitUnsignedNumber(UnsignedNumber unsignedNumber) {
-        return null;
+        return new CONST(unsignedNumber.value);
     }
 
     @Override
     public Object VisitBinaryArithmeticExpression(BinaryArithmeticExpression binaryArithmeticExpression) {
+        //TODO
+        Exp lf = (Exp) binaryArithmeticExpression.left.accept(this);
+        Exp rt = (Exp) binaryArithmeticExpression.right.accept(this);
+
+        if (binaryArithmeticExpression.op == BinaryArithmeticOperator.PLUS) {
+            //return new BINOP(lf.unEx(), rt.unEx(), BINOP.PLUS);
+        }
+
         return null;
     }
 
     @Override
     public Object VisitBinaryBooleanExpression(BinaryBooleanExpression binaryBooleanExpression) {
-        return null;
+        Exp lf = (Exp) binaryBooleanExpression.left.accept(this);
+        Exp rt = (Exp) binaryBooleanExpression.right.accept(this);
+        if (binaryBooleanExpression.op == BinaryBooleanOperator.AND) {
+            return new Cx() {
+                public Stm unCx(Label t, Label f) {
+                    Label t1 = new Label();
+                    return new SEQ(lf.unCx(t1, f),
+                            new SEQ(new LABEL(t1),
+                                    rt.unCx(t, f) ) );
+                }
+            };
+        } else { // op == OR
+            return new Cx() {
+                public Stm unCx (Label t, Label f) {
+                    Label f1 = new Label();
+                    return new SEQ(lf.unCx(t, f1),
+                            new SEQ(new LABEL(f1),
+                                    rt.unCx(t, f)));
+                }
+            };
+        }
     }
 
     @Override
@@ -74,7 +126,7 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitBooleanLiteral(BooleanLiteral booleanLiteral) {
-        return null;
+        return booleanLiteral.value? new Ex(new CONST(1)) : new Ex(new CONST(0));
     }
 
     @Override
@@ -89,7 +141,12 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitNotExpression(NotExpression notExpression) {
-        return null;
+        Exp eb = (Exp) notExpression.exp.accept(this);
+        return new Cx() {
+            public Stm unCx(Label t, Label f) {
+                return eb.unCx(f, t);
+            }
+        };
     }
 
     @Override
@@ -109,6 +166,7 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitStringLiteral(StringLiteral stringLiteral) {
+        // TODO Auto-generated method stub
         return null;
     }
 
@@ -119,42 +177,84 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitArray(Array array) {
-        return null;
+        OrdinalRep rangeRep = (OrdinalRep) array.range.accept(this);
+        Rep elemRep = (Rep) array.elemTy.accept(this);
+        return new ArrayRep(rangeRep.lower, rangeRep.upper, elemRep);
     }
 
     @Override
     public Object VisitEnumeratedType(EnumeratedType enumeratedType) {
-        return null;
+        int i=0;
+        for (String nm : enumeratedType.newConstants)
+            try {
+                env.put(nm, new Cons(i++));
+            } catch (AlreadyBoundException e) {
+                e.printStackTrace();
+            }
+        return new OrdinalRep(0, i);
     }
 
     @Override
     public Object VisitPrimitiveType(PrimitiveType primitiveType) {
-        return null;
+        if (primitiveType == PrimitiveType.INTEGER )
+            return OrdinalRep.INTEGER_REP;
+        else if (primitiveType == PrimitiveType.CHAR )
+            return OrdinalRep.CHAR_REP;
+        else
+            return OrdinalRep.BOOLEAN_REP;
     }
 
     @Override
     public Object VisitSubRangeType(SubrangeType subrangeType) {
-        return null;
+        int low = (Integer) subrangeType.low.accept(this);
+        int high = (Integer) subrangeType.high.accept(this);
+        return new OrdinalRep(low, high);
     }
 
     @Override
     public Object VisitTypeDefinition(TypeDefinition typeDefinition) {
+        Rep rep= (Rep) typeDefinition.ty.accept(this);
+        try {
+            env.put(typeDefinition.id, rep);
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Object VisitTypeId(IdType idType) {
-        return null;
+        return env.get(idType.id);
     }
 
     @Override
     public Object VisitBlock(Block block) {
+        List<SEQ> seqs = new ArrayList<>();
+        for (TypeDefinition tyDef: block.typeDefs)
+            tyDef.accept(this);
+        for (VariableDeclaration varDec: block.varDecs)
+            varDec.accept(this);
+        for (ProcedureOrFunctionDeclaration pfDec: block.subprogs) {
+            SubprogramSegment subp = (SubprogramSegment) pfDec.accept(this);
+            seqs.add(new SEQ(new LABEL(subp.label), subp.body));
+        }
+        //TODO RETURN SEQS IN A SEQUENCIAL WAY, ADD BODY ACCEPT TO LIST AND ADD LABEL OF PROGRAM TOO - SHOW SHOW
+        block.body.accept(this);
         return null;
     }
 
     @Override
     public Object VisitProgram(Program program) {
-        return null;
+        LABEL l = new LABEL(new Label("$"+"Program__"+program.id));
+        env.beginScope();
+        SEQ prog = new SEQ(l, (Stm) program.block.accept(this));
+        try {
+            env.endScope();
+        } catch (InvalidLevelException e) {
+            e.printStackTrace();
+        }
+
+        return prog;
     }
 
     @Override
@@ -189,7 +289,28 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitProcedureDeclaration(ProcedureDeclaration procedureDeclaration) {
-        return null;
+        Frame callerFrame = currentFrame;
+        Label procLabel = new Label("$" + procedureDeclaration.nm + "_" + currentLevel);
+        try {
+            env.put(procedureDeclaration.nm, new SubprogramLabel(procLabel));
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+        currentLevel++;
+        env.beginScope();
+        currentFrame = new Frame();
+        for (FormalParameter formal: procedureDeclaration.formals )
+            formal.accept(this);
+        Stm bdExp = (Stm) procedureDeclaration.body.accept(this);
+        SubprogramSegment subp = new SubprogramSegment(procLabel, currentFrame, bdExp);
+        try {
+            env.endScope();
+        } catch (InvalidLevelException e) {
+            e.printStackTrace();
+        }
+        currentLevel--;
+        currentFrame = callerFrame;
+        return subp;
     }
 
     @Override
@@ -214,31 +335,58 @@ public class TreeTranslator implements PascalVisitor {
 
     @Override
     public Object VisitIfStm(IfStatement ifStm) {
+
         return null;
     }
 
     @Override
     public Object VisitLabeledStm(LabeledStatement lblStm) {
-        return null;
+        Label l = new Label("Label " + lblStm.label + "_" + currentLevel);
+        Stm stm = (Stm) lblStm.stm.accept(this);
+        return new SEQ(new LABEL(l), stm);
     }
 
     @Override
     public Object VisitProcedureStm(ProcedureStatement procedureStm) {
-        return null;
+        List<Expr> exprList = new ArrayList<>();
+        Label nm = ((SubprogramSegment) env.get(procedureStm.name)).label;
+        exprList.add(new CONST(0)); // Static Link is always in loc 0 of frame
+        for (Expression exp : procedureStm.actuals) {
+            if (exp.accept(this) instanceof Ex)
+                exprList.add(((Ex) exp.accept(this)).unEx());
+            else
+                exprList.add((Expr) exp.accept(this));
+        }
+        return new CALL(new NAME(nm),exprList);
     }
 
     @Override
     public Object VisitWhileStm(WhileStatement whileStm) {
-        return null;
+        LABEL start = new LABEL(new Label()), body = new LABEL(new Label()), done = new LABEL(new Label());
+        CJUMP cond_jmp = new CJUMP(CJUMP.EQ, (Expr) whileStm.condition.accept(this),
+                                                                        new CONST(1), body.label, done.label);
+        JUMP jmp_start = new JUMP(start.label);
+        return new SEQ(start,
+                new SEQ(cond_jmp,
+                        new SEQ(body,
+                                new SEQ(jmp_start,
+                                        done))));
     }
 
     @Override
     public Object VisitIdExpression(IdExpression idExpression) {
-        return null;
+        return env.get(idExpression.name);
     }
 
     @Override
     public Object VisitVariableDeclaration(VariableDeclaration variableDeclaration) {
+        Rep rep = (Rep) variableDeclaration.ty.accept(this);
+        int loc = currentFrame.alloc(rep.size());
+        try {
+            env.put(variableDeclaration.id, new Var(loc, rep, currentLevel));
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
